10e81430eca2e902a62b4edbbf68e90efa4bed2c
Mike Kasberg <kasberg.mike@gmail.com>
2021-06-14 21:19:26 -0600

    Break PS1 into segments
    No changes to the prompt, but it's now broken up into functions that are
    easier to manage.
    
    I tried several powerline-style prompts and didn't fall in love with any
    of them. But something that they seem to get right is the ability to
    customize and easily reorder segments of the PS1 prompt. This commit
    moves prompt segments into their own functions so it's easier to
    understand and easier to work with a small piece.

--- executable_dot_mkasberg_bash_prompt.sh
+++ executable_dot_mkasberg_bash_prompt.sh
@@ -1,19 +1,61 @@
 #!/usr/bin/env bash
 
-__mk_generate_ps1() {
+# Different functions generate different parts (segments) of the PS1 prompt.
+# Each function should leave the colors in a clean state (e.g. call reset if they changed any colors).
+
+__mkps1_debian_chroot() {
+    # This string is intentionally single-quoted:
+    # It will be evaluated when $PS1 is evaluated to generate the prompt each time.
+    echo '${debian_chroot:+($debian_chroot)}';
+}
+
+__mkps1_username() {
     local CYAN=`tput setaf 45`;
-    local RED=`tput setaf 1`;
-    local YELLOW=`tput setaf 11`;
-    local GREEN=`tput setaf 34`;
-    local BLUE=`tput setaf 27`;
-    local MAGENTA=`tput setaf 213`;
-    local WHITE=`tput setaf 7`;
     local RESET=`tput sgr0`;
+
+    echo "\[${CYAN}\]\u\[${RESET}\]";
+}
+
+__mkps1_arrows() {
     local BOLD=`tput bold`;
-    local PS1="\n\[${CYAN}\]\${debian_chroot:+(\$debian_chroot)}\u\[${RESET}${BOLD}\] \[${RED}\]>>\[${GREEN}\]> \[${CYAN}\]\w\[${RESET}\]\$(__git_ps1 ' (\[${MAGENTA}\]%s\[${RESET}\])')\n\[${BOLD}\]\$\[${RESET}\] ";
+    local RED=`tput setaf 1`;
+    local GREEN=`tput setaf 34`;
+    local RESET=`tput sgr0`;
+
+    echo "\[${BOLD}${RED}\]>>\[${GREEN}\]>\[${RESET}\]";
+}
+
+__mkps1_workdir() {
+    local BOLD=`tput bold`;
+    local CYAN=`tput setaf 45`;
+    local RESET=`tput sgr0`;
+
+    echo "\[${BOLD}${CYAN}\]\w\[${RESET}\]";
+}
+
+__mkps1_git() {
+    local MAGENTA=`tput setaf 213`;
+    local RESET=`tput sgr0`;
+
+    # Escaping the $ is intentional:
+    # This is evaluated when the prompt is generated.
+    echo "\$(__git_ps1 ' (\[${MAGENTA}\]%s\[${RESET}\])')"
+}
+
+__mkps1_user_prompt() {
+    local BOLD=`tput bold`;
+    local RESET=`tput sgr0`;
+    
+    echo "\[${BOLD}\]\$\[${RESET}\] ";
+}
+
+__mkps1() {
+    local PS1="\n$(__mkps1_debian_chroot)$(__mkps1_username) $(__mkps1_arrows) $(__mkps1_workdir)$(__mkps1_git)\n$(__mkps1_user_prompt)";
 
     echo "$PS1";
 }
 
+# To test, for example, print output before changes and after changes
+# and see if the diff is correct.
 # Uncomment for testing:
-#__mk_generate_ps1;
+#__mkps1;
