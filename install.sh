412bb861e9c00a56bf33a4d84a5123f784f7a7e5
Mike Kasberg <kasberg.mike@gmail.com>
2023-02-25 10:56:17 -0700

    Non-interactive install script
    Allow install.sh to actually be non-interactive (no prompts)

--- install.sh
+++ install.sh
@@ -6,6 +6,21 @@
 
 set -e # exit on error
 
+install_apps=N
+quiet=N
+
+# See https://man7.org/linux/man-pages/man1/getopts.1p.html
+while getopts aq opt; do
+  case $opt in
+    a)
+      install_apps=y
+      ;;
+    q)
+      quiet=y
+      ;;
+  esac
+done
+
 if [ ! "$(command -v chezmoi)" ]; then
   bin_dir="$HOME/bin"
   chezmoi="$bin_dir/chezmoi"
@@ -24,4 +39,8 @@ fi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 # exec: replace current process with chezmoi init
-exec "$chezmoi" init --apply "--source=$script_dir"
+if [ "$quiet" == "y" ]; then
+  exec "$chezmoi" init --apply "--source=$script_dir" --promptString "Email for .gitconfig=kasberg.mike@gmail.com,Store secrets on this machine (y/N)=N,Install applications (y/N)=$install_apps"
+else
+  exec "$chezmoi" init --apply "--source=$script_dir"
+fi
