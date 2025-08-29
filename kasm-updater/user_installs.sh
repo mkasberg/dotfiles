73694de4743f1e99c1ed521d2184fedabcb81723
Mike Kasberg <kasberg.mike@gmail.com>
2025-08-28 20:04:58 -0600

    Make kasm-updater exit zsh cleanly

--- kasm-updater/user_installs.sh
+++ kasm-updater/user_installs.sh
@@ -18,7 +18,7 @@ fi
 if [ -d "${HOME}/.oh-my-zsh" ]; then
     echo "✅ Oh My Zsh is at: ${HOME}/.oh-my-zsh"
     # Needs to be in zsh, and needs to be in interactive mode to source ~/.zshrc
-    zsh -i -c "omz update"
+    zsh -i -c "omz update; exit"
 else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 fi
